b1b510d52588dce9f2c917695c8a51c8a9b51117
Mariq12 <fmlabanda@utpl.edu.ec>
2024-05-25 02:41:53 -0500

    Creación del Header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import { useState } from 'react';
 import Header from './components/header/Header';
-import Form from './components/form/Form';
-import MyOrg from './components/my-org/MyOrg';
-
 
 function App() {
-
-  const [showForm, updateShow] = useState(true)
-
-  // Operador Ternario --> condicion ? seMuestra : noSeMuestra
-  // Cortocircuito
-  // Condición && seMuestra
-  const changeShow = () => {
-    updateShow(!showForm)
-  } 
-
-    return (
-      <>
-      <Header/>
-      {/*showForm ? <Form/> : <></>*/}
-      {showForm && <Form/>}
-      <MyOrg changeShow={changeShow} />
-      </>
-    );
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
